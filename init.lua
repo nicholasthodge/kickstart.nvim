ac1f80179d653d58450b15276722955268e43896
Nicholas Hodge <nicholasthodge@gmail.com>
2025-11-13 02:40:22 -0500

    Update init.lua - Add theme

--- init.lua
+++ init.lua
@@ -881,20 +881,43 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {},
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      -- values shown are defaults and will be used if not provided
+      require('gruvbox-material').setup {
+        italics = true, -- enable italics in general
+        contrast = 'medium', -- set contrast, can be any of "hard", "medium", "soft"
+        comments = {
+          italics = true, -- enable italic comments
         },
+        background = {
+          transparent = false, -- set the background to be opaque
+        },
+        float = {
+          force_background = false, -- set to true to force backgrounds on floats even when
+          -- background.transparent is set
+          background_color = nil, -- set color for float backgrounds. If nil, uses the default color set
+          -- by the color scheme
+        },
+        signs = {
+          force_background = false, -- set to true to force backgrounds on signs even when
+          -- background.transparent is set
+          background_color = nil, -- set color for sign backgrounds. If nil, uses the default color set
+          -- by the color scheme
+        },
+        customize = nil, -- customize the theme in any way you desire, see below what this
+        -- configuration accepts
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
